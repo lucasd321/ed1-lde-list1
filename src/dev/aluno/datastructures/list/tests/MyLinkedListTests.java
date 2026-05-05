d3344dff41183bbd366ce83669ea14f05da5262a
Lucas Daniel Cunha <107688203+lucasd321@users.noreply.github.com>
2026-05-05 17:42:21 -0300

    Update and rename u to MyLinkedListTests.java

---
+++ src/dev/aluno/datastructures/list/tests/MyLinkedListTests.java
@@ -0,0 +1,10 @@
+package dev.aluno.datastructures.list.tests;
+
+import dev.aluno.datastructures.list.MyLinkedList;
+
+public class MyLinkedListTests {
+    public static void main(String[] args) {
+        MyLinkedList<String> list = new MyLinkedList<>();
+        System.out.println("List created with size: " + list.size());
+    }
+}
